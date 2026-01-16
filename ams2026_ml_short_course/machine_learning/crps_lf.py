f1ba6fb040ca9f15a8b1b650871d9717833fd0a0
thunderhoser <lagerqui@ualberta.ca>
2026-01-16 14:13:10 -0700

    FOO

--- ams2026_ml_short_course/machine_learning/crps_lf.py
+++ ams2026_ml_short_course/machine_learning/crps_lf.py
@@ -215,11 +215,7 @@ def setup_cnn(
         )(this_input_layer_object)
 
         if i == num_dense_layers - 1:
-            pass
-            # layer_object = utils._get_activation_layer(
-            #     function_name=output_activ_function_name,
-            #     slope_param=output_activ_function_alpha
-            # )(layer_object)
+            layer_object = keras.layers.Activation('softplus')(layer_object)
         else:
             layer_object = utils._get_activation_layer(
                 function_name=inner_activ_function_name,
